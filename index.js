144f5f2ae67054d38a6577f8e53a1202460051c0
CavalliB <Cerdopigman12@hotmail.com>
2025-09-02 01:10:58 -0300

    Ejercicio 3.6

--- index.js
+++ index.js
@@ -71,6 +71,11 @@ app.post("/api/persons", (request, response) => {
       error: "name or number missing",
     });
   }
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
   const person = {
     id: generateId(),
